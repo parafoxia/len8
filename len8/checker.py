c052448a62f370f9d0781937bba2b1cbe5503eef
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2021-08-01 14:38:24 -0700

    Separate logic into 2 functions

--- len8/checker.py
+++ len8/checker.py
@@ -3,39 +3,28 @@ import os
 from .errors import BadLines
 
 
-def check(path, exclude=[], extend=False):
+def check(path, exclude=[], extend=False, file_=False):
     bad_lines = []
     in_docs = False
 
     for subdir, _, files in os.walk(path):
         if not any((e for e in exclude if e in subdir)):
-            for file in filter(
-                    lambda f: f.endswith(
-                        (".py", ".pyw")
-                    ) and f not in exclude, files
-            ):
-                io = open("%s/%s" % (subdir, file))
+            if file_:
+                valid = filter(
+                    lambda f: f.endswith(file_) and f not in exclude,
+                    files
+                )
 
-                for i, line in enumerate(io):
-                    ls = line.lstrip()
-                    rs = line.rstrip()
+            else:
+                valid = filter(
+                    lambda f: f.endswith((".py", ".pyw")) and f not in exclude,
+                    files
+                )
 
-                    if ls.startswith('"""'):
-                        in_docs = True
-
-                    limit = 72 if in_docs or ls.startswith("#") else (
-                        99 if extend else 79
-                    )
-                    chars = len(rs)
-                    if chars > limit:
-                        bad_lines.append(
-                            ("%s/%s" % (subdir, file), i + 1, chars, limit)
-                        )
-
-                    if rs.endswith('"""'):
-                        in_docs = False
-
-                io.close()
+            for file in valid:
+                bad_lines, in_docs = validate_file(
+                    subdir, file, extend, bad_lines, in_docs
+                )
 
     if bad_lines:
         raise BadLines(
@@ -47,3 +36,30 @@ def check(path, exclude=[], extend=False):
         )
 
     return True
+
+
+def validate_file(subdir, file, extend, bad_lines, in_docs):
+    io = open("%s/%s" % (subdir, file))
+
+    for i, line in enumerate(io):
+        ls = line.lstrip()
+        rs = line.rstrip()
+
+        if ls.startswith('"""'):
+            in_docs = True
+
+        limit = 72 if in_docs or ls.startswith("#") else (
+            99 if extend else 79
+        )
+        chars = len(rs)
+        if chars > limit:
+            bad_lines.append(
+                ("%s/%s" % (subdir, file), i + 1, chars, limit)
+            )
+
+        if rs.endswith('"""'):
+            in_docs = False
+
+    io.close()
+
+    return bad_lines, in_docs
