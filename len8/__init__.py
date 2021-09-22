519a00c4451993dc89e928bb46ecd72ac0277309
Ethan Henderson <ethan.henderson.1998@gmail.com>
2021-09-22 23:53:25 +0100

    Bump version number

--- len8/__init__.py
+++ len8/__init__.py
@@ -27,7 +27,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 __productname__ = "len8"
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __description__ = "A utility for keeping line lengths within PEP 8 standards."
 __url__ = "https://github.com/parafoxia/len8"
 __docs__ = "https://len8.readthedocs.io/en/latest/"
