3254c282cedb753f34f840fbe283608755360380
Ethan Henderson <ethan.henderson.1998@gmail.com>
2021-11-03 15:10:38 +0000

    Update tests and licensing

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,40 @@
+# Copyright (c) 2021, Ethan Henderson, Jonxslays
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import os
 import typing as t
+from pathlib import Path
 
 import nox
+
+PROJECT_NAME = "len8"
+LIB_DIR = Path(__file__).parent / PROJECT_NAME
+TEST_DIR = Path(__file__).parent / "tests"
 
 
 def parse_requirements(path: str) -> t.List[str]:
@@ -10,18 +44,44 @@ def parse_requirements(path: str) -> t.List[str]:
 
 
 DEPS = {
-    dep.split("==")[0]: dep
+    dep.split("~=")[0]: dep
     for dep in [
         *parse_requirements("./requirements-dev.txt"),
-        *parse_requirements("./requirements-test.txt"),
+        *parse_requirements("./requirements-nox.txt"),
     ]
 }
 
 
 @nox.session(reuse_venv=True)
 def tests(session: nox.Session) -> None:
-    session.install("-U", "-r", "./requirements-test.txt")
-    session.run("pytest", "-s", "--verbose", "--log-level=INFO")
+    session.install("-U", "-r", "./requirements-nox.txt")
+    session.run(
+        "coverage",
+        "run",
+        "--omit",
+        "tests/*",
+        "-m",
+        "pytest",
+        "--testdox",
+        "--log-level=INFO",
+    )
+
+
+@nox.session(reuse_venv=True)
+def check_coverage(session: nox.Session) -> None:
+    session.install("-U", DEPS["coverage"])
+
+    if not os.path.isfile(Path(__file__).parent / ".coverage"):
+        session.skip("No coverage to check")
+
+    session.run("coverage", "report", "-m")
+
+
+# @nox.session(reuse_venv=True)
+# def check_docs_build(session: nox.Session) -> None:
+#     session.install("-U", DEPS["sphinx"], DEPS["furo"], ".")
+#     session.cd("./docs")
+#     session.run("make", "html")
 
 
 @nox.session(reuse_venv=True)
@@ -31,6 +91,45 @@ def check_formatting(session: nox.Session) -> None:
 
 
 @nox.session(reuse_venv=True)
+def check_imports(session: nox.Session) -> None:
+    session.install("-U", DEPS["flake8"], DEPS["isort"])
+    # flake8 doesn't use the gitignore so we have to be explicit.
+    session.run(
+        "flake8",
+        PROJECT_NAME,
+        "tests",
+        "--select",
+        "F4",
+        "--extend-ignore",
+        "E,F,W",
+        "--extend-exclude",
+        "__init__.py",
+    )
+    session.run("isort", ".", "-cq", "--profile", "black")
+
+
+@nox.session(reuse_venv=True)
 def check_typing(session: nox.Session) -> None:
     session.install("-U", DEPS["pyright"])
     session.run("pyright")
+
+
+@nox.session(reuse_venv=True)
+def check_licensing(session: nox.Session) -> None:
+    missing: list[Path] = []
+
+    for p in [
+        *LIB_DIR.rglob("*.py"),
+        *TEST_DIR.glob("*.py"),
+        Path(__file__),
+        Path(__file__).parent / "setup.py",
+    ]:
+        with open(p) as f:
+            if not f.read().startswith("# Copyright (c)"):
+                missing.append(p)
+
+    if missing:
+        session.error(
+            f"\n{len(missing):,} file(s) are missing their licenses:\n"
+            + "\n".join(f" - {file}" for file in missing)
+        )
