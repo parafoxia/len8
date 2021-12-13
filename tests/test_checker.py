9de6e0b28a14691676494a620e2a555e4c64fbc8
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2021-12-12 20:03:49 -0700

    Add tests for Config

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -35,6 +35,8 @@ from len8.errors import BadLines, InvalidPath
 
 TEST_FILE = Path(__file__).parent / "testdata.py"
 TEST_NON_VALID = TEST_FILE.parent / "nsx_simple_app.nsx"
+TEST_TOML_CONFIG = TEST_FILE.parent / "test.toml"
+TEST_LOL_TOML_CONFIG = TEST_FILE.parent / "test_lol.toml"
 
 
 @pytest.fixture()  # type: ignore
@@ -52,6 +54,11 @@ def extended_checker() -> len8.Checker:
 @pytest.fixture()  # type: ignore
 def custom_checker() -> len8.Checker:
     return len8.Checker(max_code_length=100, max_docs_length=80)
+
+
+@pytest.fixture()  # type: ignore
+def valid_config() -> len8.Config:
+    return len8.Config(TEST_TOML_CONFIG)
 
 
 def test_default_init(default_checker: len8.Checker) -> None:
@@ -141,7 +148,7 @@ def test_non_strict_output(default_checker: len8.Checker) -> None:
 
 
 def test_non_strict_output_extended(default_checker: len8.Checker) -> None:
-    default_checker.extend = True
+    default_checker.extend = 2
     output = (
         "2 line(s) are too long:\n"
         f"- {TEST_FILE}, line 4 (76/72)\n"
@@ -208,3 +215,73 @@ def test_skip_invalid_files(default_checker: len8.Checker) -> None:
         default_checker.check(TEST_NON_VALID)
     except UnicodeDecodeError:
         pytest.fail()
+
+
+def test__check_dir(default_checker: len8.Checker) -> None:
+    default_checker._check(Path("tests"))
+
+
+def test_valid_config_init(valid_config: len8.Config) -> None:
+    assert isinstance(valid_config, len8.Config)
+    assert valid_config.include == ["tests/testdata.py"]
+    assert valid_config.exclude == ["tests/exclude.py"]
+    assert valid_config.code_length == 88
+    assert valid_config.docs_length == 69
+    assert valid_config.strict is True
+    assert valid_config.is_configured
+
+
+def test_invalid_config_init() -> None:
+    with pytest.raises(len8.ConfigurationError) as e:
+        _ = len8.Config(TEST_NON_VALID)
+
+    assert str(e.value) == (
+        f"'{TEST_NON_VALID}' is not a valid configuration file."
+    )
+
+
+def test_config_missing_len8() -> None:
+    config = len8.Config(TEST_LOL_TOML_CONFIG)
+
+    assert not config.is_configured
+    assert config.strict is False
+    assert config.docs_length is None
+    assert config.code_length is None
+    assert config.include is None
+    assert config.exclude is None
+
+
+def test_config_bad_toml_syntax() -> None:
+    p = "./tests/invalid.toml"
+    with open(p, "w") as f:
+        f.write("[tool.invalid_toml_syntax\n")
+
+    with pytest.raises(len8.ConfigurationError) as e:
+        _ = len8.Config(p)
+        assert "Failed to parse configuration file" in str(e.value)
+
+    Path(p).unlink()
+
+
+def test_checker_from_invalid_config() -> None:
+    with pytest.raises(len8.ConfigurationError) as e:
+        _ = len8.Checker.from_config(TEST_NON_VALID)
+
+    assert str(e.value) == (
+        f"'{TEST_NON_VALID}' is not a valid configuration file."
+    )
+
+
+def test_checker_from_valid_config(valid_config: len8.Config) -> None:
+    checker = len8.Checker.from_config(valid_config)
+
+    assert checker.code_length == 88
+    assert checker.docs_length == 69
+    assert checker.extend == 0
+    assert checker.strict is True
+    assert checker.exclude == [
+        Path(".nox"),
+        Path(".venv"),
+        Path("venv"),
+        Path("tests/exclude.py"),
+    ]
