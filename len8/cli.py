c1d976f822d870258fff0dbc46713d412a94b77f
Ethan Henderson <ethan.henderson.1998@gmail.com>
2022-02-06 18:38:15 +0000

    Remove erronous print statement (#21)
    Closes #21.

--- len8/cli.py
+++ len8/cli.py
@@ -98,7 +98,6 @@ def len8(
         cfg = Config(config)
 
     except ConfigurationError as e:
-        print(e)
         checker = Checker(
             exclude=exclude,
             extend=min(extend_length, 2),
